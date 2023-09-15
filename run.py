590653ff26b47588f253799ef6d48fa9b19c4889
shencheng1021 <565072153@qq.com>
2023-09-15 17:13:15 +0800

    解决jenkins执行无法发现包的问题

--- run.py
+++ run.py
@@ -6,8 +6,15 @@
 @description: test
 @time: 2022/4/13 9:34
 """
-import os
 import unittest
+
+import sys
+import os
+curPath=os.path.abspath(os.path.dirname(__file__))
+rootPath=os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+
 
 if __name__ == '__main__':
     print(os.path.dirname(__file__))
